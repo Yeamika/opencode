81c817993f29b3bc991fd9be34aca0b2cced143d
Yeamika <3491806852@qq.com>
2026-04-02 03:05:35 +0800

    add soft workspace reload flow and native reload tool

--- packages/opencode/src/tool/registry.ts
+++ packages/opencode/src/tool/registry.ts
@@ -27,6 +27,7 @@ import { Log } from "@/util/log"
 import { LspTool } from "./lsp"
 import { Truncate } from "./truncate"
 import { ApplyPatchTool } from "./apply_patch"
+import { ReloadTool } from "./reload"
 import { Glob } from "../util/glob"
 import { pathToFileURL } from "url"
 import { Effect, Layer, ServiceMap } from "effect"
@@ -146,6 +147,7 @@ export namespace ToolRegistry {
           invalid,
           ...(question ? [ask] : []),
           bash,
+          reload,
           read,
           glob,
           grep,
